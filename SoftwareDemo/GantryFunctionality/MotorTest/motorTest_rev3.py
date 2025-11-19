ba2cab00ea85c2c49b0105defb9274ee6a00d08f
Vincent <vguo7708@gmail.com>
2025-11-18 18:49:55 -0600

    Update motorTest_rev3.py

--- SoftwareDemo/GantryFunctionality/MotorTest/motorTest_rev3.py
+++ SoftwareDemo/GantryFunctionality/MotorTest/motorTest_rev3.py
@@ -8,7 +8,7 @@ PUL_PIN_Y = 12    # Pulse pin y-axis
 DIR_PIN_Y = 16    # Direction pins y-axis
 
 duty_cycle = 0.75  # 50% duty cycle for PWM
-motor_speed = 100 #speed of motor in frequency (Hz)
+motor_speed = 1000 #speed of motor in frequency (Hz)
 
 # Initialize the pins as output devices
 pulX = PWMOutputDevice(PUL_PIN_X, active_high=True, initial_value=duty_cycle, frequency=motor_speed, pin_factory= None)  # PWM for pulse control
@@ -16,27 +16,29 @@ dirX = DigitalOutputDevice(DIR_PIN_X, active_high=True, pin_factory= None)  # Ac
 pulY = PWMOutputDevice(PUL_PIN_Y, active_high=True, initial_value=duty_cycle, frequency=motor_speed, pin_factory= None)  # PWM for pulse control
 dirY = DigitalOutputDevice(DIR_PIN_Y, active_high=True, pin_factory= None)  # Active high to rotate CW
 
-print("Starting Y-axis CW rotation...")
-dirY.off() # Set direction to CW
+# print("Test starting in 3 seconds...")
+
+print("Starting Y-axis CW rotation (up)...")
+dirY.on() # Set direction to CW
 pulY.pulse(fade_in_time=0.5, fade_out_time=0.5, n= 5, background=False) # Start PWM signal
 
-# print("Starting X-axis CW rotation...")
-# dirX.off() # Set direction to CW
+# print("Starting Y-axis CCW rotation (down)...")
+# dirY.off() # Set direction to CCW
+# pulY.pulse(fade_in_time=0.5, fade_out_time=0.5, n= 5, background=False) # Start PWM signal
+
+# print("Starting X-axis CW rotation (right)...")
+# dirX.on() # Set direction to CW
 # pulX.pulse(fade_in_time=0.5, fade_out_time=0.5, n= 5, background=False) # Start PWM signal
 
-print("Starting Y-axis CCW rotation...")
-dirY.on() # Set direction to CCW
-pulY.pulse(fade_in_time=0.5, fade_out_time=0.5, n= 5, background=False) # Start PWM signal
-
-#print("Starting X-axis CCW rotation...")
-#dirX.on() # Set direction to CCW
-#pulX.pulse(fade_in_time=0.5, fade_out_time=0.5, n= 5, background=False) # Start PWM signal
-
-# End of test
-print("Test complete.")
+# print("Starting X-axis CCW rotation (left)...")
+# dirX.off() # Set direction to CCW
+# pulX.pulse(fade_in_time=0.5, fade_out_time=0.5, n= 5, background=False) # Start PWM signal
 
 # Cleanup
 pulX.close()
 dirX.close()
 pulY.close()
 dirY.close()
+
+# End of test
+print("Test complete.")
