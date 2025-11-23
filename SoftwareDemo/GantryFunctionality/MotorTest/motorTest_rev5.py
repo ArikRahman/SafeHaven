20c5b3f682c4240f2032b06f85fcfb86d0e5eec0
CorbanF <91996663+CorbanF@users.noreply.github.com>
2025-11-22 18:53:55 -0600

    Hardcoded motor speed values to variables for easier adjustments.

--- SoftwareDemo/GantryFunctionality/MotorTest/motorTest_rev5.py
+++ SoftwareDemo/GantryFunctionality/MotorTest/motorTest_rev5.py
@@ -1,7 +1,7 @@
 # Revision 5 by Vincent
     # Executes path list
 
-from gpiozero import OutputDevice, DigitalOutputDevice, PWMOutputDevice
+from gpiozero import DigitalOutputDevice, PWMOutputDevice
 from time import sleep, time
 
 # Define the GPIO pins
@@ -10,16 +10,32 @@ DIR_PIN_X = 6     # Direction pins x-axis
 PUL_PIN_Y = 12    # Pulse pin y-axis
 DIR_PIN_Y = 16    # Direction pins y-axis
 
+
 # Parameters
 duty_cycle = 0.50  # 50% duty cycle for PWM
-motor_speed = 3000 #speed of motor in frequency (Hz)
-xUnit = 14.0 / 10000.0
-yUnit = 33.0 / 10000.0
+f_x = 64000 # PWM frequency for X-axis in Hz
+f_y = 32000 # PWM frequency for Y-axis in Hz
+
+steps_per_rev = 32000  # Microsteps per revolution for the motor, dictated by driver settings
+length_per_rev = 5  # Length per revolution in mm
+total_distance = 675  # Total traveling distance in mm for both axes
+total_pixels = 10000  # Total pixels for both axes
+
+# X-axis speed calculations
+speedX_rev_per_s = f_x / steps_per_rev  # Speed in revolutions per second
+speedX_mm_per_s = (speedX_rev_per_s) * length_per_rev  # Speed in mm/s
+speedX_pixels_per_s = (speedX_mm_per_s / total_distance) * total_pixels  # Speed in pixels/s
+
+# Y-axis speed calculations
+speedY_rev_per_s = f_y / steps_per_rev  # Speed in revolutions per second
+speedY_mm_per_s = (speedY_rev_per_s) * length_per_rev  # Speed in mm/s
+speedY_pixels_per_s = (speedY_mm_per_s / total_distance) * total_pixels  # Speed in pixels/s
+
 
 # Initialize the pins as output devices
-pulX = PWMOutputDevice(PUL_PIN_X, active_high=True, initial_value=0, frequency=motor_speed, pin_factory= None)  # PWM for pulse control
+pulX = PWMOutputDevice(PUL_PIN_X, active_high=True, initial_value=0, frequency=f_x, pin_factory= None)  # PWM for pulse control
 dirX = DigitalOutputDevice(DIR_PIN_X, active_high=True, pin_factory= None)  # Active high to rotate CW
-pulY = PWMOutputDevice(PUL_PIN_Y, active_high=True, initial_value=0, frequency=motor_speed, pin_factory= None)  # PWM for pulse control
+pulY = PWMOutputDevice(PUL_PIN_Y, active_high=True, initial_value=0, frequency=f_y, pin_factory= None)  # PWM for pulse control
 dirY = DigitalOutputDevice(DIR_PIN_Y, active_high=True, pin_factory= None)  # Active high to rotate CW
 
 # Vector List
@@ -30,32 +46,32 @@ vectorList = [
     (6750, 125), (7000, 125), (7000, 9708), (7000, 10000), (0, 10000)
 ]
 
-def up(duration):
+def up(pixels):
     print("Starting Y-axis CW rotation (up)...")
     dirY.on() # Set direction to CW
     pulY.value = duty_cycle
-    sleep(duration) # Seconds
+    sleep(abs(pixels)/speedY_pixels_per_s) # Seconds
     pulY.value = 0
 
-def down(duration):
+def down(pixels):
     print("Starting Y-axis CCW rotation (down)...")
     dirY.off() # Set direction to CCW
     pulY.value = duty_cycle
-    sleep(duration) # Seconds
+    sleep(abs(pixels)/speedY_pixels_per_s) # Seconds
     pulY.value = 0
 
-def right(duration):
+def right(pixels):
     print("Starting X-axis CW rotation (right)...")
     dirX.on() # Set direction to CW
     pulX.value = duty_cycle
-    sleep(duration) # Seconds
+    sleep(abs(pixels)/speedX_pixels_per_s) # Seconds
     pulX.value = 0
 
-def left(duration):
+def left(pixels):
     print("Starting X-axis CCW rotation (left)...")
     dirX.off() # Set direction to CCW
     pulX.value = duty_cycle
-    sleep(duration) # Seconds
+    sleep(abs(pixels)/speedX_pixels_per_s) # Seconds
     pulX.value = 0
 
 def diagonal(X, Y): # Coordinates in seconds
@@ -120,22 +136,20 @@ def followSnakepath(coords):
 
         # Horizontal
         if dx != 0:
-            durationX = abs(dx) * xUnit
             if dx > 0:
-                right(durationX)
+                right(dx)
                 sleep(0.25)
             else:
-                left(durationX)
+                left(dx)
                 sleep(0.25)
 
         # Vertical
         if dy != 0:
-            durationY = abs(dy) * yUnit
             if dy > 0:
-                up(durationY)
+                up(dy)
                 sleep(0.25)
             else:
-                down(durationY)
+                down(dy)
                 sleep(0.25)
 
         currentX, currentY = nextX, nextY
